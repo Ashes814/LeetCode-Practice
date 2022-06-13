7bd624ce54c243e97d992e38d84fca7b2435a83a
Owen <1534661514@qq.com>
2022-06-13 22:49:48 +0800

    preparing for leetcode

---
+++ 2.Add_Two_Numbers.py
@@ -0,0 +1,19 @@
+#Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        pass
+        
+
+test1_l1 = [2,4,3]
+test2_l2 = [5,6,4]
+
+
+test2_l1 = [0]
+test2_l2 = [0]
+
+test3_l1 = [9,9,9,9,9,9,9]
+test3_l2 = [9,9,9,9]
