71ecc610bbf73fb1aa3a92d4ad38c590bb3361ef
Owen <1534661514@qq.com>
2022-08-22 19:44:02 +0800

    link test

--- JavaScript/Sort/Medium/test.js
+++ JavaScript/Sort/Medium/test.js
@@ -3,3 +3,20 @@
  
 // };
 
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+}
+
+const sortList = function (head) {
+    return head
+}
+
+
+const head1 = [4, 2, 1, 3];
+const head2 = [-1, 5, 3, 4, 0];
+const head3 = [];
+
+console.log(sortList(head1));
+console.log(sortList(head2));
+console.log(sortList(head3));
