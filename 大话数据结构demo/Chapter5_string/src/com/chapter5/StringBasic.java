e47f182242d098e7f9516aa0e9792c1b39a3ca12
Ashes814 <1534661514@qq.com>
2023-06-05 11:49:58 +0800

    implement basic match String

--- 大话数据结构demo/Chapter5_string/src/com/chapter5/StringBasic.java
+++ 大话数据结构demo/Chapter5_string/src/com/chapter5/StringBasic.java
@@ -5,4 +5,63 @@ package com.chapter5;
  * @version 1.0
  */
 public class StringBasic {
+    public static void main(String[] args) {
+        String testS = "abcdefghi";
+        String testT = "sss";
+        int value = CustomString.indexSequence(testS, testT, 0);
+        System.out.println(value);
+
+    }
+}
+
+
+class CustomString {
+    private String data;
+
+    public CustomString(String data) {
+        this.data = data;
+    }
+
+    public static int index(String s, String t, int pos) {
+        int n, m, i;
+        String sub;
+        if (pos >= 0) {
+            n = s.length();
+            m = t.length();
+            i = pos;
+            while (i <= n - m - 1) {
+                sub = s.substring(i, i + m);
+
+                if (sub.compareTo(t) != 0) {
+                    ++i;
+                } else {
+                    return i;
+                }
+
+            }
+        }
+        return -1;
+    }
+
+    public static int indexSequence(String s, String t, int pos) {
+        int sLength = s.length();
+        int tLength = t.length();
+
+        for (int i = 0; i <= s.length() - 1;) {
+            for (int j = 0; j <= t.length() - 1;) {
+                if (t.substring(j, j + 1).equals(s.substring(i, i + 1))) {
+                    if (j == t.length() - 1) {
+                        return i - t.length() + 1;
+                    }
+                    i++;
+                    j++;
+
+                } else {
+                    i = i - j + 1;
+                    break;
+                }
+            }
+        }
+        return -1;
+    }
 }
