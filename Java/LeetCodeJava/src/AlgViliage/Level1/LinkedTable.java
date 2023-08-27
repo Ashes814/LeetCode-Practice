4c46b9b6db4d2eab9d3a4e58984ea77df5cf51f0
Ashes814 <1534661514@qq.com>
2023-08-27 19:54:55 +0800

    update leet code node version

--- Java/LeetCodeJava/src/AlgViliage/Level1/LinkedTable.java
+++ Java/LeetCodeJava/src/AlgViliage/Level1/LinkedTable.java
@@ -1,12 +1,14 @@
 package AlgViliage.Level1;
 
+import java.util.List;
+
 /**
  * @author 欧欧
  * @version 1.0
  */
 public class LinkedTable {
 
-    public static int getListLenth(ListNode head) {
+    public static int getListLength(ListNode head) {
         int length = 0;
         ListNode node = head;
         while (head.getNext() != null) {
@@ -17,6 +19,65 @@ public class LinkedTable {
     }
 
     public static ListNode insertNode(ListNode head, ListNode newNode, int pos) {
+        if (head == null) {
+            return newNode;
+        }
+        if (pos == 1) {
+            newNode.setNext(head);
+            head = newNode;
+            return head;
+        }
+
+        int size = LinkedTable.getListLength(head);
+        if (size + 1 < pos) {
+            System.out.println("pos out of range");
+        }
+
+
+        ListNode node = head;
+
+        for (int i = 1; i < pos - 1; i++) {
+
+
+
+            node = node.getNext();
+            i++;
+        }
+
+        newNode.setNext(node.getNext());
+        node.setNext(newNode);
+        return head;
+
+    }
+    public static ListNode deleteNode(ListNode head, int pos) {
+        if (head == null) {
+            return null;
+        }
+        int size = LinkedTable.getListLength(head);
+
+        if (pos == 1) {
+            head = head.getNext();
+            return head;
+        }
+
+        if (size < pos || pos < 1) {
+            System.out.println("Out of range");
+            return head;
+        }
+
+
+        ListNode node = head;
+
+        for (int i = 1; i < pos - 1; i++) {
+
+
+            node = node.getNext();
+            i++;
+        }
+
+        node.setNext(node.getNext().getNext());
+
+        return head;
 
     }
 }
---
+++ Java/LeetCodeJava/src/AlgViliage/Level1/ListNodeLeetCode.java
@@ -0,0 +1,18 @@
+package AlgViliage.Level1;
+
+/**
+ * @author 欧欧
+ * @version 1.0
+ */
+public class ListNodeLeetCode {
+}
+
+class ListNodeLC {
+    public int val;
+    public ListNode next;
+
+    ListNodeLC(int x) {
+        val = x;
+        next = null;
+    }
+}
