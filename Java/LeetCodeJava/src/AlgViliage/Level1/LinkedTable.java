e57522188dfb3736669154296489c428110656eb
Ashes814 <1534661514@qq.com>
2023-08-28 13:56:50 +0800

    update linked list silver worker

--- Java/LeetCodeJava/src/AlgViliage/Level1/LinkedTable.java
+++ Java/LeetCodeJava/src/AlgViliage/Level1/LinkedTable.java
@@ -8,101 +8,101 @@ import java.util.List;
  */
 public class LinkedTable {
 
-    public static int getListLength(ListNode head) {
-        int length = 0;
-        ListNode node = head;
-        while (head.getNext() != null) {
-            length++;
-            node.setNext(node.getNext());
-        }
-        return length;
-    }
-
-    public static ListNode insertNode(ListNode head, ListNode newNode, int pos) {
-        if (head == null) {
-            return newNode;
-        }
-        if (pos == 1) {
-            newNode.setNext(head);
-            head = newNode;
-            return head;
-        }
-
-        int size = LinkedTable.getListLength(head);
-        if (size + 1 < pos) {
-            System.out.println("pos out of range");
-        }
-
-
-        ListNode node = head;
-
-        for (int i = 1; i < pos - 1; i++) {
-
-
-
-            node = node.getNext();
-            i++;
-        }
-
-        newNode.setNext(node.getNext());
-        node.setNext(newNode);
-        return head;
-
-    }
-    public static ListNode deleteNode(ListNode head, int pos) {
-        if (head == null) {
-            return null;
-        }
-        int size = LinkedTable.getListLength(head);
-
-        if (pos == 1) {
-            head = head.getNext();
-            return head;
-        }
-
-        if (size < pos || pos < 1) {
-            System.out.println("Out of range");
-            return head;
-        }
-
-
-        ListNode node = head;
-
-        for (int i = 1; i < pos - 1; i++) {
-
-
-            node = node.getNext();
-            i++;
-        }
-
-        node.setNext(node.getNext().getNext());
-
-        return head;
-
-    }
+//    public static int getListLength(ListNode head) {
+//        int length = 0;
+//        ListNode node = head;
+//        while (head.getNext() != null) {
+//            length++;
+//            node.setNext(node.getNext());
+//        }
+//        return length;
+//    }
+//
+//    public static ListNode insertNode(ListNode head, ListNode newNode, int pos) {
+//        if (head == null) {
+//            return newNode;
+//        }
+//        if (pos == 1) {
+//            newNode.setNext(head);
+//            head = newNode;
+//            return head;
+//        }
+//
+//        int size = LinkedTable.getListLength(head);
+//        if (size + 1 < pos) {
+//            System.out.println("pos out of range");
+//        }
+//
+//
+//        ListNode node = head;
+//
+//        for (int i = 1; i < pos - 1; i++) {
+//
+//
+//
+//            node = node.getNext();
+//            i++;
+//        }
+//
+//        newNode.setNext(node.getNext());
+//        node.setNext(newNode);
+//        return head;
+//
+//    }
+//    public static ListNode deleteNode(ListNode head, int pos) {
+//        if (head == null) {
+//            return null;
+//        }
+//        int size = LinkedTable.getListLength(head);
+//
+//        if (pos == 1) {
+//            head = head.getNext();
+//            return head;
+//        }
+//
+//        if (size < pos || pos < 1) {
+//            System.out.println("Out of range");
+//            return head;
+//        }
+//
+//
+//        ListNode node = head;
+//
+//        for (int i = 1; i < pos - 1; i++) {
+//
+//
+//            node = node.getNext();
+//            i++;
+//        }
+//
+//        node.setNext(node.getNext().getNext());
+//
+//        return head;
+//
+//    }
 }
 
-class ListNode {
-    private int val;
-    private ListNode next;
-
-    public ListNode(int val) {
-        this.val = val;
-    }
-
-    public int getVal() {
-        return val;
-    }
-
-    public void setVal(int val) {
-        this.val = val;
-    }
-
-    public ListNode getNext() {
-        return next;
-    }
-
-    public void setNext(ListNode next) {
-        this.next = next;
-    }
-}
+//class ListNode {
+//    private int val;
+//    private ListNode next;
+//
+//    public ListNode(int val) {
+//        this.val = val;
+//    }
+//
+//    public int getVal() {
+//        return val;
+//    }
+//
+//    public void setVal(int val) {
+//        this.val = val;
+//    }
+//
+//    public ListNode getNext() {
+//        return next;
+//    }
+//
+//    public void setNext(ListNode next) {
+//        this.next = next;
+//    }
+//}
--- Java/LeetCodeJava/src/AlgViliage/Level1/ListNodeLeetCode.java
+++ Java/LeetCodeJava/src/AlgViliage/Level1/ListNodeLeetCode.java
@@ -1,15 +1,84 @@
 package AlgViliage.Level1;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
 /**
  * @author 欧欧
  * @version 1.0
  */
 public class ListNodeLeetCode {
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNodeLC findFirstCommonNodeBySet(ListNodeLC headA, ListNodeLC headB) {
+        Set<ListNodeLC> set = new HashSet<>();
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+
+        }
+
+        while (headB != null){
+            if (set.contains(headB)) {
+                return headB;
+            } else {
+                headB = headB.next;
+            }
+        }
+        return null;
+    }
+    public static ListNodeLC findFirstCommonNodeByStack(ListNodeLC headA, ListNodeLC headB) {
+        Stack<ListNodeLC> aStack =new Stack();
+        Stack<ListNodeLC> bStack =new Stack();
+
+        while (headA != null) {
+            aStack.push(headA);
+            headA = headA.next;
+        }
+        while (headB != null) {
+            bStack.push(headB);
+            headB = headB.next;
+        }
+
+        while (!aStack.isEmpty() && !bStack.isEmpty()) {
+            ListNodeLC nodeA = aStack.pop();
+            ListNodeLC nodeB = bStack.pop();
+            if (nodeA == nodeB) {
+                return nodeA;
+            }
+        }
+
+        return null;
+    }
+
+    public static boolean backTestValidation(ListNodeLC head) {
+        ListNodeLC temp = head;
+        Stack<Integer> stack = new Stack();
+        int listLengh = 0;
+        while (temp != null) {
+            stack.push(temp.val);
+            listLengh++;
+        }
+        temp = head;
+        for (int i = 0; i < listLengh / 2; i++) {
+            if (stack.pop() != temp.val) {
+                return false;
+            }
+            temp = temp.next;
+
+        }
+        return true;
+
+
+    }
 }
 
 class ListNodeLC {
     public int val;
-    public ListNode next;
+    public ListNodeLC next;
 
     ListNodeLC(int x) {
         val = x;
