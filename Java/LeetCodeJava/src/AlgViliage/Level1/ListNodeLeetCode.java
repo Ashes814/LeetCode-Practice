6330dbfa4fd72f096ae821163f13d30fc4e38af3
Ashes814 <1534661514@qq.com>
2023-08-30 11:37:17 +0800

    linked list delete and find common

--- Java/LeetCodeJava/src/AlgViliage/Level1/ListNodeLeetCode.java
+++ Java/LeetCodeJava/src/AlgViliage/Level1/ListNodeLeetCode.java
@@ -177,6 +177,102 @@ public class ListNodeLeetCode {
         }
         return slow;
     }
+    public static ListNodeLC rotateRight(ListNodeLC head, int k) {
+        ListNodeLC temp = head;
+        int size = 0;
+        while(temp != null) {
+            temp = temp.next;
+            size++;
+        }
+        if (k > size) {
+            k = k % size;
+        }
+       ListNodeLC front = head;
+       ListNodeLC end = getKthFromEnd(head, k);
+        for (int i = 1; i <= size - k; i++) {
+            front = front.next;
+        }
+        front.next = null;
+        ListNodeLC endTemp = end;
+        while(endTemp != null) {
+            endTemp = endTemp.next;
+        }
+        endTemp.next = front;
+        return end;
+
+
+    }
+    public static ListNodeLC removeElements(ListNodeLC head, int val) {
+        ListNodeLC dummyHead = new ListNodeLC(-1);
+        dummyHead.next = head;
+        ListNodeLC cur = dummyHead;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummyHead.next;
+    }
+    public static ListNodeLC removeNthFromEnd(ListNodeLC head, int n) {
+        ListNodeLC dummy = new ListNodeLC(-1);
+        dummy.next = head;
+        ListNodeLC fast = head;
+        ListNodeLC second = dummy;
+        for (int i = 1; i <= n; i++) {
+            fast = fast.next;
+
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+
+    public static ListNodeLC deleteDuplicates(ListNodeLC head) {
+        if (head == null) {
+            return head;
+        }
+        ListNodeLC dummy = new ListNodeLC(0);
+        dummy.next = head;
+        ListNodeLC cur = dummy;
+        while(cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    public static ListNodeLC findFirstCommonNode(ListNodeLC pHead1, ListNodeLC pHead2) {
+        if (pHead1 == null || pHead2 == null) {
+            return null;
+        }
+        ListNodeLC p1 = pHead1;
+        ListNodeLC p2 = pHead2;
+        while (p1 != p2) {
+            p1 = p1.next;
+            p2 = p2.next;
+            if (p1 != p2) {
+                if (p1 == null) {
+                    p1 = pHead2;
+                }
+                if (p2 == null) {
+                    p2 = pHead1;
+                }
+            }
+        }
+        return p1;
+
+    }
+    public static ListNodeLC findFirstCommonNode2(ListNodeLC pHead1, ListNodeLC pHead2) {}
+
 }
 
 class ListNodeLC {
