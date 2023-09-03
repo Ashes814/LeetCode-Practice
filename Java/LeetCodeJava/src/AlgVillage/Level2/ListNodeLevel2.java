adaa2450c19d5222c0684c5176e90d1af77a3182
Ashes814 <1534661514@qq.com>
2023-09-03 12:13:44 +0800

    reverse and swap linked list

--- Java/LeetCodeJava/src/AlgVillage/Level2/ListNodeLevel2.java
+++ Java/LeetCodeJava/src/AlgVillage/Level2/ListNodeLevel2.java
@@ -12,10 +12,10 @@ public class ListNodeLevel2 {
                         new ListNode(2,
                         new ListNode(3,
                         new ListNode(4,
-                        new ListNode(5)))));
+                        null))));
 
 
-        showList(reverseListWithRecur(head));
+        showList(swapPairs(head));
     }
 
     public static void showList(ListNode head) {
@@ -65,6 +65,78 @@ public class ListNodeLevel2 {
 
 
     };
+
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+
+
+        }
+        return dummyNode.next;
+    }
+    public static ListNode reverseBetween2(ListNode head, int left, int right) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+
+        }
+
+        ListNode rightNode= pre;
+        for (int i = 1; i <= right - left; i++) {
+            rightNode = rightNode.next;
+        };
+        ListNode leftNode = pre.next;
+        ListNode succ = rightNode.next;
+        rightNode.next = null;
+        reverseList(leftNode);
+        pre.next = rightNode;
+        leftNode.next = succ;
+        return dummyNode.next;
+
+    }
+    public static ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode pre = dummyHead;
+        ListNode left = pre.next;
+        ListNode right = left.next;
+
+        while (pre.next.next != null) {
+            left.next = right.next;
+            right.next = left;
+            pre.next = right;
+
+            pre = pre.next.next;
+            if (pre.next == null) {
+                break;
+            } else {
+                left = pre.next;
+                right = left.next;
+            }
+
+        }
+        return dummyHead.next;
+        // swap
+
+
+    }
 
 }
 
