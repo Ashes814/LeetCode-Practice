517e96accd3308fb2377d18fc857811ec98db9b7
Ashes814 <1534661514@qq.com>
2023-09-05 08:58:36 +0800

    is palindrome 2

--- Java/LeetCodeJava/src/AlgVillage/Level2/ListNodeLevel2.java
+++ Java/LeetCodeJava/src/AlgVillage/Level2/ListNodeLevel2.java
@@ -213,6 +213,36 @@ public class ListNodeLevel2 {
             return res.next;
         }
     }
+    public static boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode pre = head;
+        ListNode prepre = null;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+
+            pre.next = prepre;
+            prepre = pre;
+        }
+
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        while (pre != null && slow != null) {
+            if (pre.val != slow.val) {
+                return false;
+            }
+            pre = pre.next;
+            slow = slow.next;
+        }
+        return true;
+    }
 
 }
 
