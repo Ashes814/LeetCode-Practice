e063404f0e8d5761ebe7da6ec64609df179cbdb5
Ashes814 <1534661514@qq.com>
2023-09-21 09:23:25 +0800

    RPN

--- Java/LeetCodeJava/src/AlgVillage/level4/StackLevel.java
+++ Java/LeetCodeJava/src/AlgVillage/level4/StackLevel.java
@@ -90,6 +90,33 @@ public class StackLevel {
 
 
     }
+
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token: tokens) {
+            if (!Character.isDigit(token.charAt(0)) && token.length() == 1) {
+                int b = stack.pop();
+                int a = stack.pop();
+                switch (token) {
+                    case "+":
+                        stack.push(a + b);
+                        break;
+                    case "-":
+                        stack.push(a - b);
+                        break;
+                    case "*":
+                        stack.push(a * b);
+                        break;
+                    case "/":
+                        stack.push(a / b);
+                        break;
+                }
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
 }
 
 class MinStack {
