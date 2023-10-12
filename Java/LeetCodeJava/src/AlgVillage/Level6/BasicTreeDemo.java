749abcf6148fb148057750412ff40b5b09692bd6
Ashes814 <1534661514@qq.com>
2023-10-12 10:28:18 +0800

    recursion to traversal

--- Java/LeetCodeJava/src/AlgVillage/Level6/BasicTreeDemo.java
+++ Java/LeetCodeJava/src/AlgVillage/Level6/BasicTreeDemo.java
@@ -408,7 +408,51 @@ public class BasicTreeDemo {
 
     }
 
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        preOrder(root, res);
+        return res;
 
+
+
+
+    }
+
+    public void preOrder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        preOrder(root.left, res);
+        preOrder(root.right, res);
+    } public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        preOrder(root, res);
+        return res;
+
+
+
+
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inOrder(root, res);
+        return res;
+
+
+
+
+    }
+
+    public void inOrder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+
+        inOrder(root.left, res);
+        res.add(root.val);
+        inOrder(root.right, res);
+    }
 
 }
 
