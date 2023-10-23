befe69d8b737feaafbfe6b3204359394cd481a4f
Ashes814 <1534661514@qq.com>
2023-10-23 10:38:17 +0800

    pass leetcode 617

--- Java/LeetCodeJava/src/AlgVillage/Level7/BifurTreeDFS.java
+++ Java/LeetCodeJava/src/AlgVillage/Level7/BifurTreeDFS.java
@@ -67,6 +67,22 @@ public class BifurTreeDFS {
         return check(p.left, q.right) && check(p.right, q.left);
 
     }
+
+    public static TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+
+        if (t2 == null) {
+            return t1;
+        }
+
+        TreeNode merged = new TreeNode(t1.val + t2.val);
+        merged.left = mergeTrees(t1.left, t2.left);
+        merged.right = mergeTrees(t1.right, t2.right);
+        return merged;
+
+    }
 }
 
 
