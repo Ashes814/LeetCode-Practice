28cad5a793d53b4c3b889392cb80d3a270ccad05
Ashes814 <1534661514@qq.com>
2023-10-26 17:06:50 +0800

    passleetcode 257

--- Java/LeetCodeJava/src/AlgVillage/Level7/BifurTreeDFS.java
+++ Java/LeetCodeJava/src/AlgVillage/Level7/BifurTreeDFS.java
@@ -83,6 +83,22 @@ public class BifurTreeDFS {
         return merged;
 
     }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        dfs(root, "", res);
+        return res;
+    }
+
+    void dfs(TreeNode root, String path, List<String> res) {
+        if (root == null) return;
+        if (root.left == null && root.right == null) {
+            res.add(path + root.val);
+            return;
+        }
+        dfs(root.left, path + root.val + "->", res);
+        dfs(root.right, path + root.val + "->", res);
+    }
 }
 
 
