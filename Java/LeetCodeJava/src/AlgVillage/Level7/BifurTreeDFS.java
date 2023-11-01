8a241c3d3b34ae14c116134c122d2cf8d3b99c35
Ashes814 <1534661514@qq.com>
2023-11-01 12:43:59 +0800

    update max depth in tree

--- Java/LeetCodeJava/src/AlgVillage/Level7/BifurTreeDFS.java
+++ Java/LeetCodeJava/src/AlgVillage/Level7/BifurTreeDFS.java
@@ -135,6 +135,16 @@ public class BifurTreeDFS {
 
     }
 
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+        return 1 + Math.max(leftMax, rightMax);
+    }
+
 
 }
 
