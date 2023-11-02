dfadcf4c7ad329f4e811377069e752fa05844d78
Ashes814 <1534661514@qq.com>
2023-11-02 14:59:23 +0800

    finish leetcode 104

--- Java/LeetCodeJava/src/AlgVillage/Level7/BifurTreeDFS.java
+++ Java/LeetCodeJava/src/AlgVillage/Level7/BifurTreeDFS.java
@@ -145,6 +145,23 @@ public class BifurTreeDFS {
         return 1 + Math.max(leftMax, rightMax);
     }
 
+    public boolean isBalanced(TreeNode root) {
+       return height(root) >= 0;
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
 
 }
 
