d128e881729bf9d96594062e29009081e6f28d0f
Ashes814 <1534661514@qq.com>
2023-11-08 15:52:06 +0800

    complete binary search in For Loop

--- Java/LeetCodeJava/src/AlgVillage/Level9/BiSearch.java
+++ Java/LeetCodeJava/src/AlgVillage/Level9/BiSearch.java
@@ -6,6 +6,27 @@ package AlgVillage.Level9;
  */
 public class BiSearch {
     public static void main(String[] args) {
+        int[] testArray = {1, 2, 3,4, 5,6,8,9,9,19 };
+        System.out.println(binarySearchLoop(testArray, 0, 9, 19));
+
 
     }
+
+    public static int binarySearchLoop(int[] array, int low, int high, int target) {
+        while (low <= high) {
+            int m =low + ((high - low) >> 1);
+            if (target < array[m]) {
+                high = m - 1;
+            };
+            if (target > array[m]) {
+                low = m + 1;
+            }
+            if (target == array[m]) {
+                return m;
+            }
+
+        }
+        return -1;
+    }
 }
+
