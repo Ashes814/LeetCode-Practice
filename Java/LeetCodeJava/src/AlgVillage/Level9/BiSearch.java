3b507d0f5e2b114a30f7d0493dedf21a87d249f3
Ashes814 <1534661514@qq.com>
2023-11-12 16:14:20 +0800

    update alg

--- Java/LeetCodeJava/src/AlgVillage/Level9/BiSearch.java
+++ Java/LeetCodeJava/src/AlgVillage/Level9/BiSearch.java
@@ -7,7 +7,7 @@ package AlgVillage.Level9;
 public class BiSearch {
     public static void main(String[] args) {
         int[] testArray = {1, 2, 3,4, 5,6,6,6,6,6,6,6,6,6,6,6,6,6,6,8,9,9,9,9,9,9,9,9,9,9,19 };
-        System.out.println(binarySearchRecursionEqual(testArray, 0, testArray.length, 6));
+        System.out.println(searchRange(testArray, 20)[0]);
 
 
     }
@@ -74,7 +74,39 @@ public class BiSearch {
             }
             return m;
         }
-        return binarySearchLoop(array, low, high, target);
+        return binarySearchRecursionEqual(array, low, high, target);
+
+    }
+    public static int binarySearchRecursionEqualMax(int[] array, int low, int high, int target) {
+        if (low > high) {
+            return -1;
+        }
+
+        int m =low + ((high - low) >> 1);
+        if (target < array[m]) {
+            high = m - 1;
+        };
+        if (target > array[m]) {
+            low = m + 1;
+        }
+        if (target == array[m]) {
+            for (int i = m; i < array.length; i++) {
+                if (i == array.length - 1) {
+                    return i;
+                }
+                if (target != array[i + 1]) {
+                    return i;
+                } else {
+                    continue;
+                }
+            }
+            return m;
+        }
+        return binarySearchRecursionEqualMax(array, low, high, target);
+
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
 
     }
 
