bb5f6f1934ca090f2c1b62cf743233a10a3702c2
Ashes814 <1534661514@qq.com>
2023-12-07 15:13:00 +0800

    update quick sort

--- Java/LeetCodeJava/src/AlgVillage/Level10/BronzeQuichSort.java
+++ Java/LeetCodeJava/src/AlgVillage/Level10/BronzeQuichSort.java
@@ -7,6 +7,12 @@ package AlgVillage.Level10;
 public class BronzeQuichSort {
     public static void main(String [] args) {
 
+        int[] testArray = {26, 53, 48,15,13,48,32,15};
+        quickSort(testArray, 0, testArray.length - 1);
+        for (int i = 0; i < testArray.length - 1; i++) {
+            System.out.println(testArray[i]);
+        }
+
     }
 
     public static void quickSort(int[] arr, int left, int right) {
@@ -19,11 +25,16 @@ public class BronzeQuichSort {
                     int temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
-
                 }
             }
 
+            int pivotIndex = i + 1;
+            int temp = arr[pivotIndex];
+            arr[pivotIndex] = arr[right];
+            arr[right] = temp;
 
+            quickSort(arr, left, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, right);
         }
 
     }
