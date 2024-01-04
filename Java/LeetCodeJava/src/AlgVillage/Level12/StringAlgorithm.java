bd915797b41f8d9b6f6fd277d77a24b96347757f
Ashes814 <1534661514@qq.com>
2024-01-04 21:34:59 +0800

    number to string

--- Java/LeetCodeJava/src/AlgVillage/Level12/StringAlgorithm.java
+++ Java/LeetCodeJava/src/AlgVillage/Level12/StringAlgorithm.java
@@ -9,8 +9,8 @@ import java.util.Arrays;
  */
 public class StringAlgorithm {
     public static void main(String[] args) {
-        String testString = "aBcde";
-        System.out.println(toLowerCase(testString));
+        String testString = "   aBcde";
+        System.out.println(myAtoi("       -123213"));
     }
 
     public static String toLowerCase(String s) {
@@ -31,5 +31,46 @@ public class StringAlgorithm {
 
 
         return sb.toString();
+    }
+
+    public static int myAtoi(String str) {
+       int len = str.length();
+       char[] charArray = str.toCharArray();
+
+       int index = 0;
+       while (index < len && charArray[index] == ' ') {
+           index++;
+       }
+       if (index == len) {
+           return 0;
+       }
+
+       int sign = 1;
+       char firstChar = charArray[index];
+       if (firstChar == '+') {
+           index++;
+       } else if (firstChar == '-') {
+           index++;
+           sign = -1;
+       }
+
+       int res = 0;
+       while (index < len) {
+           char currChar = charArray[index];
+           if (currChar > '9' || currChar < '0') {
+               break;
+           }
+
+           if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && (currChar - '0') > Integer.MAX_VALUE % 10)) {
+               return Integer.MAX_VALUE;
+           }
+           if (res < Integer.MIN_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && (currChar - '0') > Integer.MAX_VALUE % 10)) {
+               return Integer.MAX_VALUE;
+           }
+
+           res = res * 10 + sign * (currChar - '0');
+           index++;
+       }
+       return res;
     }
 }
