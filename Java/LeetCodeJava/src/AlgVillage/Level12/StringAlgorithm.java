523be8742298a6da93a67cf59b8e993675baeb8e
Ashes814 <1534661514@qq.com>
2024-01-10 21:28:09 +0800

    passcode 917

--- Java/LeetCodeJava/src/AlgVillage/Level12/StringAlgorithm.java
+++ Java/LeetCodeJava/src/AlgVillage/Level12/StringAlgorithm.java
@@ -10,9 +10,10 @@ import java.util.Arrays;
 public class StringAlgorithm {
     public static void main(String[] args) {
         String testString = "abcd";
-        System.out.println(reverseStr(testString, 2));
+//        System.out.println(isAlp('a') && isAlp('c'));
 //        System.out.println(myAtoi("       -123213"));
 //        reverseString(testString);
+        System.out.println(reverseOnlyLetters("Test1ng-Leet=code-Q!"));
     }
 
     public static String toLowerCase(String s) {
@@ -105,9 +106,42 @@ public class StringAlgorithm {
         while (left < right) {
             char temp = arr[left];
             arr[left] = arr[right];
-            arr[right] = arr[left];
+            arr[right] = temp;
             left++;
             right--;
         }
+    }
+
+    public static boolean isAlp(char c) {
+        if ((c <= 90 && c >= 65) || (c <= 122 && c >= 97)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static String reverseOnlyLetters(String s) {
+        char[] sArray = s.toCharArray();
+        int head = 0;
+        int tail = sArray.length - 1;
+        char temp;
+        while (tail > head) {
+            if (isAlp(sArray[head]) && isAlp(sArray[tail])) {
+                temp = sArray[head];
+                sArray[head] = sArray[tail];
+                sArray[tail] = temp;
+            } else if (isAlp(sArray[head]) && !isAlp(sArray[tail])) {
+                tail--;
+                continue;
+            } else if (!isAlp(sArray[head]) && isAlp(sArray[tail])) {
+                head++;
+                continue;
+            }
+            head++;
+            tail--;
+
+        }
+        return new String(sArray);
     }
 }
