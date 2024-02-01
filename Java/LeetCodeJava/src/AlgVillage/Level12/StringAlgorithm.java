154cd64940674ae72e0fd3aac52c804d7b43a7b4
Ashes814 <1534661514@qq.com>
2024-02-01 19:40:46 +0800

    pass leet code 443

--- Java/LeetCodeJava/src/AlgVillage/Level12/StringAlgorithm.java
+++ Java/LeetCodeJava/src/AlgVillage/Level12/StringAlgorithm.java
@@ -293,4 +293,38 @@ public class StringAlgorithm {
        return strs[0];
 
     }
+
+    public static int compress(char[] chars) {
+        int n = chars.length;
+        int write = 0, left = 0;
+        for (int read = 0; read < n; read++) {
+            if (read == n - 1 || chars[read] != chars[read + 1]) {
+                chars[write++] = chars[read];
+                int num = read - left + 1;
+                if (num > 1) {
+                    int anchor = write;
+                    while (num > 0) {
+                        chars[write++] = (char) (num % 10 + '0');
+                        num /= 10;
+
+                    }
+                    reverse2(chars, anchor, write - 1);
+                }
+                left = read + 1;
+            }
+        }
+        return write;
+
+
+    }
+
+    public void reverse2(char[] chars, int left, int right) {
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+    }
 }
