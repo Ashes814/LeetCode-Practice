43054a31a7862d5d0b77d25257b5a10f960e365d
Ashes814 <1534661514@qq.com>
2024-02-02 20:49:02 +0800

    passcode 1822 sign productive

--- Java/LeetCodeJava/src/AlgVillage/Level13/MathAlgorithms.java
+++ Java/LeetCodeJava/src/AlgVillage/Level13/MathAlgorithms.java
@@ -5,4 +5,27 @@ package AlgVillage.Level13;
  * @version 1.0
  */
 public class MathAlgorithms {
+    public static void main(String[] args) {
+
+    }
+
+    public static int arraySign(int[] nums) {
+        int negCount = 0;
+        for (int n: nums) {
+            if (n == 0) {
+                return 0;
+            }
+            if (n < 0) {
+                negCount++;
+            }
+        }
+
+        if ((negCount % 2) == 0) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+
 }
