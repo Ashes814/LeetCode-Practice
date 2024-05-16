514c92de42079c5d2c6ca31f189c8bae4415e08f
Ashes814 <1534661514@qq.com>
2024-05-16 09:45:22 +0800

    finish level 15

--- Java/LeetCodeJava/src/AlgVillage/Level14/HeatTest.java
+++ Java/LeetCodeJava/src/AlgVillage/Level14/HeatTest.java
@@ -1,10 +1,20 @@
 package AlgVillage.Level14;
+
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class HeatTest {
     public static void main(String[] args) {
 
     }
+
+     public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
 
     public int findKthLargest(int[] nums, int k) {
         if (k > nums.length) {
@@ -25,7 +35,31 @@ public class HeatTest {
             }
         }
         return minHeap.peek();
+    };
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+
+        PriorityQueue<ListNode> q = new PriorityQueue<>(Comparator.comparing(node->node.val));
+        for (int i = 0; i < lists.length; i++) {
+            q.add(lists[i]);
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (!q.isEmpty()) {
+            tail.next = q.poll();
+            tail = tail.next;
+            if (tail.next != null) {
+                q.add(tail.next);
+            }
+        }
+
+        return dummy.next;
     }
+
+
 
 
 }
