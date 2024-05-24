60d62bf433eec629819dc8d65afb6a637f902875
Ashes814 <1534661514@qq.com>
2024-05-24 21:18:30 +0800

    implement find the length of longest string two distinct

--- Java/LeetCodeJava/src/AlgVillage/Level16/MovingWindow.java
+++ Java/LeetCodeJava/src/AlgVillage/Level16/MovingWindow.java
@@ -1,5 +1,6 @@
 package AlgVillage.Level16;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,7 +9,8 @@ public class MovingWindow {
         int[] testArray = {1, 12, 13, 15, 5, 3};
 //        System.out.println(findMaxAverage(testArray, 4));
 //        System.out.println(findLengthOfLCIS(testArray));
-        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+//        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstringTwoDistinct("aabbcccd"));
     }
 
     public static double calAverage(int start, int k, int[] nums) {
@@ -102,6 +104,31 @@ public class MovingWindow {
         }
 
         return res;
+    }
+
+    public static int lengthOfLongestSubstringTwoDistinct(String s) {
+        if (s.length() < 3) {
+            return s.length();
+        }
+        int left = 0, right = 0;
+        HashMap<Character, Integer> hashmap = new HashMap<>();
+        int maxLen = 2;
+
+        while (right < s.length()) {
+            if (hashmap.size() < 3) {
+                hashmap.put(s.charAt(right), right++);
+            }
+
+            if (hashmap.size() == 3) {
+                int del_idx = Collections.min(hashmap.values());
+                hashmap.remove(s.charAt(del_idx));
+                left = del_idx + 1;
+            }
+
+            maxLen = Math.max(maxLen, right - left);
+        }
+
+        return maxLen;
     }
 
 
