46d299ea613976edc2c3ef4f7eafecca9c8c5e9a
Ashes814 <1534661514@qq.com>
2024-05-26 20:09:09 +0800

    pass leetcode 209 the min array

--- Java/LeetCodeJava/src/AlgVillage/Level16/MovingWindow.java
+++ Java/LeetCodeJava/src/AlgVillage/Level16/MovingWindow.java
@@ -131,5 +131,18 @@ public class MovingWindow {
         return maxLen;
     }
 
+    public static int minSubArrayLen(int target, int[] nums) {
+       int left = 0, right = 0, sum = 0, min = Integer.MAX_VALUE;
+       while (right < nums.length) {
+           sum += nums[right++];
+           while (sum >= target) {
+               min = Math.min(min, right - left);
+               sum -= nums[left++];
+           }
+       }
+       return min == Integer.MAX_VALUE ? 0 : min;
+
+    }
+
 
 }
