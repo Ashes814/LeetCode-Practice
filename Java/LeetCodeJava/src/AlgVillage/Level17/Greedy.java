ab24944b7c647d309c93ac9be1e26a50d7e779dc
Ashes814 <1534661514@qq.com>
2024-06-01 12:26:21 +0800

    pass leetcode 135

--- Java/LeetCodeJava/src/AlgVillage/Level17/Greedy.java
+++ Java/LeetCodeJava/src/AlgVillage/Level17/Greedy.java
@@ -79,4 +79,30 @@ public class Greedy {
         }
         return true;
     }
+
+    public static int candy(int[] ratings) {
+        int[] candyVec = new int[ratings.length];
+        candyVec[0] = 1;
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] <= ratings[i - 1]) {
+                candyVec[i] = 1;
+            } else {
+                candyVec[i] = candyVec[i - 1] + 1;
+            }
+        }
+
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candyVec[i] = Math.max(candyVec[i], candyVec[i + 1] + 1);
+            }
+        }
+
+        int res = 0;
+        for (int i: candyVec) {
+            res += i;
+        }
+
+        return res;
+
+    }
 }
