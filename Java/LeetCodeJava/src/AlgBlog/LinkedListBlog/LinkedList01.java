588dd95ee001368aa1d4605552c6bde35c1f8bbd
Ashes814 <1534661514@qq.com>
2024-06-27 18:50:32 +0800

    update leetcode linked list

--- Java/LeetCodeJava/src/AlgBlog/LinkedListBlog/LinkedList01.java
+++ Java/LeetCodeJava/src/AlgBlog/LinkedListBlog/LinkedList01.java
@@ -1,6 +1,12 @@
 package AlgBlog.LinkedListBlog;
 
 public class LinkedList01 {
+    static int arr[] = new int[5];
+
+    public static void main(String[] args) {
+        System.out.println(arr[0]);
+        System.out.println(1 + "10" + 3 + "2");
+    }
 }
 
 class LinkedListDemo {
@@ -16,6 +22,39 @@ class LinkedListDemo {
         }
         return length;
     }
+
+    public static ListNode insertNode(ListNode head, ListNode nodeInsert, int position) {
+
+        // head 为空，则随便插入，直接返回要插入的Node
+        if (head == null) {
+            return nodeInsert;
+        }
+
+        // 处理越界情况
+        int size = getListLength(head);
+        if (position > size + 1 || position < 1) {
+            System.out.println("Position out of range!");
+            return head;
+        }
+
+
+        // 在表头插入
+        if (position == 1) {
+            nodeInsert.next = head;
+            return nodeInsert;
+        }
+
+        // 在其他位置插入
+        ListNode tempNode = head;
+        int count = 1;
+        while (count < position - 1) {
+            tempNode = tempNode.next;
+            count++;
+        }
+        nodeInsert.next = tempNode.next;
+        tempNode.next = nodeInsert;
+        return head;
+    }
 }
 
 
@@ -30,3 +69,5 @@ class ListNode {
         next = null;
     }
 }
+
+
