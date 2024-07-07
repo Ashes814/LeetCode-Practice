d6fcbb2bfcf30631785919e93f1b1b2828b254dd
Ashes814 <1534661514@qq.com>
2024-07-07 20:23:41 +0800

    finish fCN by a stack

--- Java/LeetCodeJava/src/AlgBlog/LinkedListBlog/LinkedList01.java
+++ Java/LeetCodeJava/src/AlgBlog/LinkedListBlog/LinkedList01.java
@@ -1,9 +1,6 @@
 package AlgBlog.LinkedListBlog;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class LinkedList01 {
     static int arr[] = new int[5];
@@ -113,6 +110,35 @@ class LinkedListDemo {
             }
         }
         return null;
+    }
+
+    // Using two stack to solve this problem
+    public static ListNode findFirstCommonNodeByStack(ListNode headA, ListNode headB) {
+        Stack<ListNode> stackA = new Stack<>();
+        Stack<ListNode> stackB = new Stack<>();
+
+        while (headA != null) {
+            stackA.push(headA);
+            headA = headA.next;
+        }
+
+        while (headB != null) {
+            stackB.push(headB);
+            headB = headB.next;
+        }
+
+
+        ListNode preNode = null;
+        while (!stackA.isEmpty() && !stackB.isEmpty()) {
+            if (stackA.peek() == stackB.peek()) {
+                preNode = stackA.pop();
+                stackB.pop();
+            } else {
+                break;
+            }
+        }
+        return preNode;
+
     }
 }
 
