2f4559a092ff3a48b1383eaac37dea74d9d3fd53
Ashes814 <1534661514@qq.com>
2024-07-30 18:05:43 +0800

    find crycle in linkedlist by pointer

--- Java/LeetCodeJava/src/AlgBlog/LinkedListBlog/LinkedList01.java
+++ Java/LeetCodeJava/src/AlgBlog/LinkedListBlog/LinkedList01.java
@@ -479,6 +479,27 @@ class LinkedListDemo {
         return null;
     }
 
+    public ListNode deteCycleWithPointer(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                fast = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+
+        }
+        return null;
+
+    }
+
 }
 
 
