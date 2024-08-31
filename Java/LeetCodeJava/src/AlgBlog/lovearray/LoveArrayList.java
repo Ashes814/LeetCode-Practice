98471fab9addcbd3295858a06ad646ba42ee4b11
Ashes814 <1534661514@qq.com>
2024-08-31 21:32:53 +0800

    pass leetcode 86

--- Java/LeetCodeJava/src/AlgBlog/lovearray/LoveArrayList.java
+++ Java/LeetCodeJava/src/AlgBlog/lovearray/LoveArrayList.java
@@ -16,8 +16,9 @@ public class LoveArrayList {
 //            System.out.print(num);
 //        }
 //        System.out.println(removeByElement(nums, 9, 9));
-        int[] nums = {2, 2, 2, 1,1};
-        System.out.println(isMonotonic(nums));
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        merge(nums1,3, nums2, 3);
 
     }
 
@@ -125,6 +126,38 @@ public class LoveArrayList {
             }
         }
         return ans;
+
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m + n - 1;
+        m--;
+        n--;
+        while (i >= 0) {
+            if (m < 0) {
+                nums1[i] = nums2[n];
+                n--;
+                i--;
+                continue;
+            }
+            if (n < 0) {
+                nums1[i] = nums1[m];
+                m--;
+                i--;
+                continue;
+            }
+            if (nums1[m] >= nums2[n]) {
+                nums1[i] = nums1[m];
+                m--;
+            } else {
+                nums1[i] = nums2[n];
+                n--;
+            }
+            i--;
+
+
+
+        }
 
     }
 }
