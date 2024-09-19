817c3beb7a7c327e32c0234452da833ecc0f5b02
Ashes814 <1534661514@qq.com>
2024-09-19 19:10:51 +0800

    pass leetcode 136

--- Java/LeetCodeJava/src/AlgBlog/lovearray/LoveArrayList.java
+++ Java/LeetCodeJava/src/AlgBlog/lovearray/LoveArrayList.java
@@ -350,6 +350,14 @@ public class LoveArrayList {
     }
 
 
+    public static int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num: nums) {
+            res = res ^ num;
+        }
+        return res;
+    }
+
 
 
 }
