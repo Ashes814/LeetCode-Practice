62f6555264858924188cae73f795175c73a75a79
Ashes814 <1534661514@qq.com>
2024-10-22 22:43:23 +0800

    pass leetcode 225

--- Java/LeetCodeJava/src/AlgBlog/HashQueue/HashQueueAlg.java
+++ Java/LeetCodeJava/src/AlgBlog/HashQueue/HashQueueAlg.java
@@ -1,6 +1,6 @@
 package AlgBlog.HashQueue;
 
-import java.util.Stack;
+import java.util.*;
 
 public class HashQueueAlg {
 }
@@ -51,4 +51,46 @@ class MyQueue {
             stack2.push(stack1.pop());
         }
     }
+}
+
+class MyStack {
+
+    Queue<Integer> queueMain;
+    Queue<Integer> queueSub;
+    public MyStack() {
+        queueMain = new LinkedList<Integer>();
+        queueSub = new LinkedList<Integer>();
+    }
+
+    public void push(int x) {
+        queueSub.add(x);
+        reverseAndInsert();
+    }
+
+    public int pop() {
+        return queueMain.poll();
+    }
+
+    public int top() {
+
+        return queueMain.peek();
+    }
+
+    public boolean empty() {
+        if (queueMain.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void reverseAndInsert() {
+        while (!queueMain.isEmpty()) {
+            queueSub.add(queueMain.poll());
+        }
+        Queue<Integer> temp = queueMain;
+        queueMain = queueSub;
+        queueSub = temp;
+    }
+
 }
