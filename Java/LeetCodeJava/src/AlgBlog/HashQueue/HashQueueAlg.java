78335c60a1a19f214b1a4f5301779f52636d1850
Ashes814 <1534661514@qq.com>
2024-10-25 16:39:09 +0800

    pass leetcode 15

--- Java/LeetCodeJava/src/AlgBlog/HashQueue/HashQueueAlg.java
+++ Java/LeetCodeJava/src/AlgBlog/HashQueue/HashQueueAlg.java
@@ -19,6 +19,54 @@ public class HashQueueAlg {
         return null;
 
     }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for (int first = 0; first < n; first++) {
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+
+            int third = n - 1;
+
+
+            for (int second = first + 1; second < n; second++) {
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+
+                int target = -nums[first] - nums[second];
+
+                while (second < third && nums[third] > target) {
+                    third--;
+                }
+
+                if (second == third) {
+                    break;
+                }
+
+                if (nums[third] == target) {
+                    ArrayList<Integer> res = new ArrayList<>();
+                    res.add(nums[first]);
+                    res.add(nums[second]);
+                    res.add(nums[third]);
+                    ans.add(res);
+                }
+
+
+
+
+            }
+
+
+        }
+
+        return ans;
+    }
 }
 
 
