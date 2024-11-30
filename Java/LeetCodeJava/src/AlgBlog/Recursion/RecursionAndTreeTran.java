f265c34821d49ab652fd7b3031d04d7d66967231
Ashes814 <1534661514@qq.com>
2024-11-30 15:38:15 +0800

    pass leetcode 144

---
+++ Java/LeetCodeJava/src/AlgBlog/Recursion/RecursionAndTreeTran.java
@@ -0,0 +1,58 @@
+package AlgBlog.Recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecursionAndTreeTran {
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+
+        res = preorder(root, res);
+        return res;
+    }
+
+    public List<Integer> preorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return res;
+        }
+
+        res.add(root.val);
+        preorder(root.left, res);
+        preorder(root.right, res);
+        return res;
+
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
