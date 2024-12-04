a901cd2a84826b4e17ed889fec7428118643300c
Ashes814 <1534661514@qq.com>
2024-12-04 18:07:12 +0800

    for implementes inorderTraversal

--- Java/LeetCodeJava/src/AlgBlog/Recursion/RecursionAndTreeTran.java
+++ Java/LeetCodeJava/src/AlgBlog/Recursion/RecursionAndTreeTran.java
@@ -1,6 +1,8 @@
 package AlgBlog.Recursion;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 public class RecursionAndTreeTran {
@@ -24,6 +26,29 @@ public class RecursionAndTreeTran {
         preorder(root.left, res);
         preorder(root.right, res);
         return res;
+
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Deque<TreeNode> stack = new LinkedList<>();
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+
+
 
     }
 }
