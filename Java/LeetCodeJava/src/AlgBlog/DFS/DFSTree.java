9011ecf71c5740e43e5b9dfb2ced87a922823f42
Ashes814 <1534661514@qq.com>
2024-12-05 19:53:19 +0800

    pass leetcode 101

--- Java/LeetCodeJava/src/AlgBlog/DFS/DFSTree.java
+++ Java/LeetCodeJava/src/AlgBlog/DFS/DFSTree.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public class DFSTree {
     public static void main(String[] args) {
-
+        TreeNode rootTest = new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3)));
+        System.out.println(isSymmetric(rootTest));
     }
 
     public static boolean isSameTree(TreeNode p, TreeNode q) {
@@ -20,6 +21,33 @@ public class DFSTree {
         }
 
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+
+    }
+
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return check(root.left, root.right);
+    }
+
+    public static boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+
+        if (p.val == q.val) {
+            return check(p.left, q.right) && check(p.right, q.left);
+        } else {
+            return false;
+        }
 
 
     }
