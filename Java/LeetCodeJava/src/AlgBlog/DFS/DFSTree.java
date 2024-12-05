c0237f9852210bacf047a6e8ffe36b1a3dc5e13b
Ashes814 <1534661514@qq.com>
2024-12-05 20:35:10 +0800

    pass leetcode 617

--- Java/LeetCodeJava/src/AlgBlog/DFS/DFSTree.java
+++ Java/LeetCodeJava/src/AlgBlog/DFS/DFSTree.java
@@ -51,6 +51,46 @@ public class DFSTree {
 
 
     }
+
+    public static TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+
+        if (root2 == null) {
+            return root1;
+        }
+
+        TreeNode merged = new TreeNode(root1.val + root2.val);
+        merged.left = mergeTrees(root1.left, root2.left);
+        merged.right = mergeTrees(root1.right, root2.right);
+        return merged;
+
+
+
+    }
+
+//    public static TreeNode mergeTreeNode(TreeNode root1, TreeNode root2, TreeNode resTree) {
+//        if (root1 == null && root2 == null) {
+//            resTree = null;
+//        }
+//
+//        if (root1 != null && root2 == null) {
+//            resTree = new TreeNode(root1.val);
+//        }
+//
+//        if (root1 == null && root2 != null) {
+//            resTree = new TreeNode(root2.val);
+//        }
+//
+//        if (root1 != null && root2 != null) {
+//            resTree =  new TreeNode(root1.val + root2.val);
+//        }
+//
+//        resTree.left = mergeTreeNode(root1.left, root2.left, resTree);
+//        resTree.right = mergeTreeNode(root1.right, root2.right, resTree);
+//
+//    }
 }
 
 
