f21be5559c848466c8392ef1360a875c3b8c3ef6
Ashes814 <1534661514@qq.com>
2024-12-06 14:14:14 +0800

    pass leetcode 110 balanced trees

--- Java/LeetCodeJava/src/AlgBlog/DFS/DFSTree.java
+++ Java/LeetCodeJava/src/AlgBlog/DFS/DFSTree.java
@@ -134,6 +134,33 @@ public class DFSTree {
         return root;
     }
 
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public static boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+
+    public static int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        };
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        if (Math.abs(leftHeight - rightHeight) > 1 || leftHeight == -1 || rightHeight == -1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    };
+
 //    public static TreeNode mergeTreeNode(TreeNode root1, TreeNode root2, TreeNode resTree) {
 //        if (root1 == null && root2 == null) {
 //            resTree = null;
