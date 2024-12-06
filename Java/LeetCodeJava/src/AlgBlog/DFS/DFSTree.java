572c210cd9ab67269874282e3742d5a8eb16875b
Ashes814 <1534661514@qq.com>
2024-12-06 13:51:32 +0800

    pass leetcode 226

--- Java/LeetCodeJava/src/AlgBlog/DFS/DFSTree.java
+++ Java/LeetCodeJava/src/AlgBlog/DFS/DFSTree.java
@@ -122,6 +122,18 @@ public class DFSTree {
 
     }
 
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode tempNode = root.left;
+        root.left = root.right;
+        root.right = tempNode;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
 //    public static TreeNode mergeTreeNode(TreeNode root1, TreeNode root2, TreeNode resTree) {
 //        if (root1 == null && root2 == null) {
 //            resTree = null;
