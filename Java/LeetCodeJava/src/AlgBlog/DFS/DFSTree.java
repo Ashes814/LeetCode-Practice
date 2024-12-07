8894e84aa199ccaa5c0b4db439f63649159d166b
Ashes814 <1534661514@qq.com>
2024-12-07 14:48:02 +0800

    pass leetcode 2369

--- Java/LeetCodeJava/src/AlgBlog/DFS/DFSTree.java
+++ Java/LeetCodeJava/src/AlgBlog/DFS/DFSTree.java
@@ -204,6 +204,30 @@ public class DFSTree {
         return maxRes + 1;
     }
 
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == null && right == null) {
+            return null;
+        }
+
+        if (left != null && right == null) {
+            return left;
+        }
+
+        if (left == null && right != null) {
+            return right;
+        }
+
+
+        return root;
+    }
+
 
 
 //    public static TreeNode mergeTreeNode(TreeNode root1, TreeNode root2, TreeNode resTree) {
