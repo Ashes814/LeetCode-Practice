07d91f30595f4cc4d05eeda7d10b913d52c5bd60
Ashes814 <1534661514@qq.com>
2024-12-07 13:58:43 +0800

    pass leetcode 111

--- Java/LeetCodeJava/src/AlgBlog/DFS/DFSTree.java
+++ Java/LeetCodeJava/src/AlgBlog/DFS/DFSTree.java
@@ -161,6 +161,31 @@ public class DFSTree {
         return Math.max(leftHeight, rightHeight) + 1;
     };
 
+    public static int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        int minDepth = Integer.MAX_VALUE;
+
+        if (root.left != null) {
+            minDepth = Math.min(minDepth(root.left), minDepth);
+        }
+
+        if (root.right != null) {
+            minDepth = Math.min(minDepth(root.right), minDepth);
+        }
+
+        return minDepth + 1;
+    }
+
+
+
+
 //    public static TreeNode mergeTreeNode(TreeNode root1, TreeNode root2, TreeNode resTree) {
 //        if (root1 == null && root2 == null) {
 //            resTree = null;
