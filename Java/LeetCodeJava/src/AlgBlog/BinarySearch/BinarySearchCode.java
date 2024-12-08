6b8e62ea7e90e559291f01fa40650b69c6bd64d8
Ashes814 <1534661514@qq.com>
2024-12-08 15:42:32 +0800

    pass leetcode 153

--- Java/LeetCodeJava/src/AlgBlog/BinarySearch/BinarySearchCode.java
+++ Java/LeetCodeJava/src/AlgBlog/BinarySearch/BinarySearchCode.java
@@ -51,4 +51,20 @@ public class BinarySearchCode {
         }
         return -1;
     }
+
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            }
+
+        }
+        return nums[left];
+    }
 }
