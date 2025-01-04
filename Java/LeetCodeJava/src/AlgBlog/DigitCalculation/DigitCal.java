bb2d82c76c22558fde1cc304c55c68e136d1b08a
Ashes814 <1534661514@qq.com>
2025-01-04 14:16:11 +0800

    pass leetcode 338

--- Java/LeetCodeJava/src/AlgBlog/DigitCalculation/DigitCal.java
+++ Java/LeetCodeJava/src/AlgBlog/DigitCalculation/DigitCal.java
@@ -16,4 +16,19 @@ public class DigitCal {
 
         return count;
     }
+
+    public static int[] countBits(int n) {
+        int[] res = new int[n+1];
+        for (int i = 0; i <= n; i++) {
+            int bit = i;
+            int ones = 0;
+            while (bit != 0) {
+                bit = bit & (bit - 1);
+                ones++;
+            }
+            res[i] = ones;
+
+        }
+        return res;
+    }
 }
