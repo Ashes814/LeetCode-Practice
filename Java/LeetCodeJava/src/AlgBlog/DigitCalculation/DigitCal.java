ea01d45457fcaa23c1e6be7f6e60b2dab312c817
Ashes814 <1534661514@qq.com>
2025-01-10 21:16:07 +0800

    finish bit calc

--- Java/LeetCodeJava/src/AlgBlog/DigitCalculation/DigitCal.java
+++ Java/LeetCodeJava/src/AlgBlog/DigitCalculation/DigitCal.java
@@ -73,4 +73,38 @@ public class DigitCal {
         return res;
     }
 
+    public void checkDuplicates(int[] array) {
+        BitSet bs = new BitSet(32000);
+        for (int i = 0; i < array.length; i++) {
+            int num = array[i];
+            int num0 = num - 1;
+            if (bs.get(num0)) {
+                System.out.println(num);
+            } else {
+                bs.set(num0);
+            }
+        }
+    }
+
+    class BitSet {
+        int[] bitSet;
+
+        public BitSet(int size) {
+            this.bitSet = new int[size>>5];
+        }
+
+        boolean get(int pos) {
+            int wordNumber = pos >> 5;
+            int bitNumber = pos & 0x1F;
+            return (bitSet[wordNumber] & (1 << bitNumber)) != 0;
+
+        }
+
+        void set(int pos) {
+            int wordNumber = pos >> 5;
+            int bitNumber = pos & 0x1F;
+            bitSet[wordNumber] |= 1 << bitNumber;
+        }
+    }
+
 }
