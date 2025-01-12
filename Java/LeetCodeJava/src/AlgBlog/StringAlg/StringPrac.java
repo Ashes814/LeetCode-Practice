ebce29c786165be466d4c4da9939d2536d396c66
Ashes814 <1534661514@qq.com>
2025-01-12 14:17:38 +0800

    pass leetcode 8

--- Java/LeetCodeJava/src/AlgBlog/StringAlg/StringPrac.java
+++ Java/LeetCodeJava/src/AlgBlog/StringAlg/StringPrac.java
@@ -17,4 +17,41 @@ public class StringPrac {
         }
         return new String(chars);
     }
+
+    public int myAtoi(String s) {
+        char[] chars = s.toCharArray();
+        int len = s.length();
+        int index = 0;
+        while (index < len && chars[index] == ' ') {
+            index++;
+        }
+
+        int sign = 1;
+        if (chars[index] == '+') {
+            index++;
+        } else if (chars[index] == '-') {
+            index++;
+            sign = -1;
+
+        }
+        int res = 0;
+        while (index < len) {
+            if (chars[index] < '0' || chars[index] > '9' ) {
+                break;
+            }
+
+
+            if ((res > Integer.MAX_VALUE / 10) || (res == Integer.MAX_VALUE / 10 && (chars[index] - '0') > (Integer.MAX_VALUE % 10))) {
+                return Integer.MAX_VALUE;
+            }
+
+            if ((res < Integer.MIN_VALUE / 10) || (res == Integer.MIN_VALUE / 10 && (chars[index] - '0') > -(Integer.MIN_VALUE % 10))) {
+                return Integer.MIN_VALUE;
+            }
+            res = res * 10 + sign * (chars[index] - '0');
+            index++;
+        }
+
+        return res;
+    }
 }
