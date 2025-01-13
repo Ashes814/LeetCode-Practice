d664b21218a2a01d2df7d667d3ef27ad327ed6f8
Ashes814 <1534661514@qq.com>
2025-01-13 15:25:22 +0800

    pass leetcode 344

--- Java/LeetCodeJava/src/AlgBlog/StringAlg/StringPrac.java
+++ Java/LeetCodeJava/src/AlgBlog/StringAlg/StringPrac.java
@@ -54,4 +54,16 @@ public class StringPrac {
 
         return res;
     }
+
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
 }
