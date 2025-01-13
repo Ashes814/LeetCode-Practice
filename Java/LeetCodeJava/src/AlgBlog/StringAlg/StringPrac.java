b8b44daae92d39d9b159b9debc9a4835bc456106
Ashes814 <1534661514@qq.com>
2025-01-13 15:46:04 +0800

    pass leetcode 917

--- Java/LeetCodeJava/src/AlgBlog/StringAlg/StringPrac.java
+++ Java/LeetCodeJava/src/AlgBlog/StringAlg/StringPrac.java
@@ -85,4 +85,26 @@ public class StringPrac {
             right--;
         }
     }
+
+    public String reverseOnlyLetters(String s) {
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (!Character.isLetter(chars[left])) {
+                left++;
+            }
+            if (!Character.isLetter(chars[right])) {
+                right--;
+            }
+            if (Character.isLetter(chars[left]) && Character.isLetter(chars[right])) {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return new String(chars);
+    }
 }
