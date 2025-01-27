4c8e6197bf3ec7aaf74644bc820672737e7b0a19
Ashes814 <1534661514@qq.com>
2025-01-27 11:26:13 +0800

    pass leetcode 16.05

--- Java/LeetCodeJava/src/AlgBlog/MathAlg/MathAlgPrac.java
+++ Java/LeetCodeJava/src/AlgBlog/MathAlg/MathAlgPrac.java
@@ -19,4 +19,12 @@ public class MathAlgPrac {
         }
         return sign;
     }
+
+    public int trailingZeroes(int n) {
+        int count = 0;
+        for (long i = 5; n / i > 0; i *= 5) {
+            count += n/i;
+        }
+        return count;
+    }
 }
