7f2b1ce2d875563a988f8e647960d93f95e78d16
Ashes814 <1534661514@qq.com>
2025-01-31 13:20:32 +0800

    pass leetcode 7

--- Java/LeetCodeJava/src/AlgBlog/MathAlg/MathAlgPrac.java
+++ Java/LeetCodeJava/src/AlgBlog/MathAlg/MathAlgPrac.java
@@ -27,4 +27,24 @@ public class MathAlgPrac {
         }
         return count;
     }
+
+    public int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            int tmp = x % 10;
+            if (res > Integer.MAX_VALUE / 10 || ((res == Integer.MAX_VALUE / 10) && (tmp > 7))) {
+                return 0;
+            }
+
+            if (res < -214748364 || ((res == -214748364) && (tmp < -8))) {
+                return 0;
+            }
+
+            res = res * 10 + tmp;
+            x = x / 10;
+
+
+        }
+        return res;
+    }
 }
