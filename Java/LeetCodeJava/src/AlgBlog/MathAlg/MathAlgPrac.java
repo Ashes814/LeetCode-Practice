40287de4aea6ebf92c012c1282d8083d4f1cd8f2
Ashes814 <1534661514@qq.com>
2025-02-01 12:03:36 +0800

    pass leetcode 8

--- Java/LeetCodeJava/src/AlgBlog/MathAlg/MathAlgPrac.java
+++ Java/LeetCodeJava/src/AlgBlog/MathAlg/MathAlgPrac.java
@@ -47,4 +47,20 @@ public class MathAlgPrac {
         }
         return res;
     }
+
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revNum = 0;
+        while (x > revNum) {
+            revNum = revNum * 10 + x % 10;
+            x = x / 10;
+        }
+        return x == revNum || x == revNum / 10;
+
+
+
+    }
 }
