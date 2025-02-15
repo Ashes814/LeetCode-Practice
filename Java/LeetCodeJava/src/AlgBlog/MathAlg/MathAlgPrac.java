3785315bcd58bd0608555cea69d524556ade0cb6
Ashes814 <1534661514@qq.com>
2025-02-15 11:30:11 +0800

    pass leetcode 231

--- Java/LeetCodeJava/src/AlgBlog/MathAlg/MathAlgPrac.java
+++ Java/LeetCodeJava/src/AlgBlog/MathAlg/MathAlgPrac.java
@@ -175,4 +175,16 @@ public class MathAlgPrac {
         ans.reverse();
         return ans.toString();
     }
+
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+
+        return n == 1;
+    }
 }
