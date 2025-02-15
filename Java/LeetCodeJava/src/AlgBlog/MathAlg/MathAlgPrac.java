9d883f7d235a83ea4009d9fbbfe8bc7101fecbb3
Ashes814 <1534661514@qq.com>
2025-02-15 11:25:15 +0800

    pass leetcode 67

--- Java/LeetCodeJava/src/AlgBlog/MathAlg/MathAlgPrac.java
+++ Java/LeetCodeJava/src/AlgBlog/MathAlg/MathAlgPrac.java
@@ -137,4 +137,42 @@ public class MathAlgPrac {
         }
         return digits;
     }
+
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int add = 0;
+        StringBuffer ans = new StringBuffer();
+        while (i >= 0 || j >= 0 || add != 0) {
+            int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int result = x + y + add;
+            ans.append(result % 10);
+            add = result / 10;
+            i--;
+            j--;
+
+        }
+        ans.reverse();
+        return ans.toString();
+    }
+
+    public String addBinary(String a, String b) {
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int add = 0;
+        StringBuffer ans = new StringBuffer();
+        while (i >= 0 || j >= 0 || add != 0) {
+            int x = i >= 0 ? a.charAt(i) - '0' : 0;
+            int y = j >= 0 ? b.charAt(j) - '0' : 0;
+            int result = x + y + add;
+            ans.append(result % 2);
+            add = result / 2;
+            i--;
+            j--;
+
+        }
+        ans.reverse();
+        return ans.toString();
+    }
 }
