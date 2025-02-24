f43f935156b7657978ca4333e81a4fb29c7c5713
Ashes814 <1534661514@qq.com>
2025-02-24 12:06:40 +0800

    pass leetcode 3

--- Java/LeetCodeJava/src/AlgBlog/MovingWindow/MovingWindowCode.java
+++ Java/LeetCodeJava/src/AlgBlog/MovingWindow/MovingWindowCode.java
@@ -1,6 +1,7 @@
 package AlgBlog.MovingWindow;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class MovingWindowCode {
     public static void main(String[] args) {
@@ -54,5 +55,20 @@ public class MovingWindowCode {
             }
         }
         return maxLength;
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) return 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        int left = 0;
+        for (int right = 0;right < s.length(); right++) {
+            if (map.containsKey(s.charAt(right))) {
+                left = Math.max(left, map.get(s.charAt(right)) + 1);
+            }
+            map.put(s.charAt(right), right);
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
     }
 }
