cd1f0850d42ae6ab496aa5f6127c675bb885270f
Ashes814 <1534661514@qq.com>
2025-02-26 14:55:17 +0800

    pass leetcode 239

--- Java/LeetCodeJava/src/AlgBlog/MovingWindow/MovingWindowCode.java
+++ Java/LeetCodeJava/src/AlgBlog/MovingWindow/MovingWindowCode.java
@@ -1,12 +1,12 @@
 package AlgBlog.MovingWindow;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.*;
 
 public class MovingWindowCode {
     public static void main(String[] args) {
-
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+        maxSlidingWindow(nums, k);
     }
 
     public double findMaxAverage(int[] nums, int k) {
@@ -143,5 +143,28 @@ public class MovingWindowCode {
 
         }
         return minLen;
+    }
+
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] != o2[0] ? o2[0] - o1[0] : o2[1] - o1[1];
+            }
+        });
+        for (int i = 0; i < k; i++) {
+            pq.offer(new int[]{nums[i], i});
+        }
+        int[] ans = new int[n - k + 1];
+        ans[0] = pq.peek()[0];
+        for (int i = k; i < n; i++) {
+            pq.offer(new int[]{nums[i], i});
+            while (pq.peek()[1] <= i - k) {
+                pq.poll();
+            }
+            ans[i - k + 1] = pq.peek()[0];
+        }
+        return ans;
     }
 }
