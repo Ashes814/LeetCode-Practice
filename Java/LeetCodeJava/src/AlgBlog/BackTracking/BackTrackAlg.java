7d6b98e5533c39d8e1156a02c852bb08164820c9
Ashes814 <1534661514@qq.com>
2025-03-09 13:26:07 +0800

    pass leetcode 39

--- Java/LeetCodeJava/src/AlgBlog/BackTracking/BackTrackAlg.java
+++ Java/LeetCodeJava/src/AlgBlog/BackTracking/BackTrackAlg.java
@@ -52,4 +52,36 @@ public class BackTrackAlg {
 
 
 
+
+
+}
+
+class CombinationSum {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        dfs(candidates, 0, target);
+        return res;
+    }
+
+    public void dfs(int[] c, int u, int target) {
+        if (target < 0) {
+            return;
+        }
+
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = u; i < c.length ; i++) {
+            if (c[i] <= target) {
+                path.add(c[i]);
+                dfs(c, i, target - c[i]);
+                path.remove(path.size() - 1);
+            }
+
+        }
+    }
 }
