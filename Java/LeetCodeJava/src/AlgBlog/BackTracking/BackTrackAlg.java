c81acba90a151f6d4db7a270d3a17b2d4c411052
Ashes814 <1534661514@qq.com>
2025-03-12 10:00:08 +0800

    pass leetcode 79

--- Java/LeetCodeJava/src/AlgBlog/BackTracking/BackTrackAlg.java
+++ Java/LeetCodeJava/src/AlgBlog/BackTracking/BackTrackAlg.java
@@ -38,7 +38,6 @@ public class BackTrackAlg {
             resultList.add(new ArrayList<>(path));
             return;
         }
-
         for (int i = startIndex; i <= n; i++) {
             path.addLast(i);
             dfs(n, k, i + 1, path, resultList);
@@ -212,4 +211,39 @@ class ABCPermute {
 
     }
 
+}
+
+class FindWord {
+    public boolean exist(char[][] board, String word) {
+        char[] words = word.toCharArray();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, words, i, j, 0)) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+    public boolean dfs(char[][] board, char[] words, int i, int j, int k) {
+        if (i >= board.length || i < 0 ||j >= board[0].length || j < 0 || board[i][j] != words[k]) {
+            return false;
+        }
+
+        if (k == words.length - 1) {
+            return true;
+        }
+
+        board[i][j] = '\0';
+
+        boolean res = dfs(board, words, i + 1, j, k + 1) ||
+                      dfs(board, words, i - 1 , j, k + 1) ||
+                      dfs(board, words, i, j + 1, k + 1) ||
+                      dfs(board, words, i, j - 1, k + 1);
+
+        board[i][j] = words[k];
+        return res;
+    }
 }
