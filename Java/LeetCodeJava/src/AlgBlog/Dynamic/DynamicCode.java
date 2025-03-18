215733e875289c7e43d95e72592e732b4f26e6a1
Ashes814 <1534661514@qq.com>
2025-03-19 07:53:10 +0800

    pass leetcode 64

--- Java/LeetCodeJava/src/AlgBlog/Dynamic/DynamicCode.java
+++ Java/LeetCodeJava/src/AlgBlog/Dynamic/DynamicCode.java
@@ -1,4 +1,75 @@
 package AlgBlog.Dynamic;
 
+import java.util.Arrays;
+
 public class DynamicCode {
+    public static void main(String[] args) {
+
+    }
+
+    public int uniquePaths(int m, int n) {
+        return search(m, n);
+
+    }
+
+    public int search(int m, int n) {
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+
+        return search(m - 1, n) + search(m, n - 1);
+    }
+
+    public int uniquePaths2(int m, int n) {
+        int[][] f = new int[m][n];
+        f[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0 && j > 0) {
+                    f[i][j] = f[i - 1][j] + f[i][j - 1];
+                } else if (i > 0) {
+                    f[i][j] = f[i - 1][j];
+                } else if (j > 0) {
+                    f[i][j] = f[i][j - 1];
+                }
+            }
+        }
+        return f[m - 1][n - 1];
+    }
+
+
+    // Using a rolling array
+    public int uniquePaths3(int m, int n) {
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] = dp[j - 1] + dp[j];
+            }
+
+        }
+        return dp[n - 1];
+    }
+
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        int[][] f = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    f[i][j] = grid[i][j];
+                } else {
+                    int top = i - 1 >= 0 ? f[i - 1][j] + grid[i][j] : Integer.MAX_VALUE;
+                    int bottom = j - 1 >= 0 ? f[j - 1][i] + grid[i][j] : Integer.MAX_VALUE;
+                    f[i][j] = Math.min(top, bottom);
+                }
+
+            }
+
+        }
+        return f[m - 1][n - 1];
+    }
 }
