b52b2ecf11f8cc7eb2e687c28ef31226bb294594
Ashes814 <1534661514@qq.com>
2025-03-19 08:13:54 +0800

    pass leetcode 120

--- Java/LeetCodeJava/src/AlgBlog/Dynamic/DynamicCode.java
+++ Java/LeetCodeJava/src/AlgBlog/Dynamic/DynamicCode.java
@@ -1,6 +1,7 @@
 package AlgBlog.Dynamic;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class DynamicCode {
     public static void main(String[] args) {
@@ -63,7 +64,7 @@ public class DynamicCode {
                     f[i][j] = grid[i][j];
                 } else {
                     int top = i - 1 >= 0 ? f[i - 1][j] + grid[i][j] : Integer.MAX_VALUE;
-                    int bottom = j - 1 >= 0 ? f[j - 1][i] + grid[i][j] : Integer.MAX_VALUE;
+                    int bottom = j - 1 >= 0 ? f[i][j - 1] + grid[i][j] : Integer.MAX_VALUE;
                     f[i][j] = Math.min(top, bottom);
                 }
 
@@ -71,5 +72,30 @@ public class DynamicCode {
 
         }
         return f[m - 1][n - 1];
+    }
+
+    public int minimumTotal(List<List<Integer>> tri) {
+        int n = tri.size();
+        int ans = Integer.MAX_VALUE;
+        int[][] f = new int[n][n];
+        f[0][0] = tri.get(0).get(0);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                int val = tri.get(i).get(j);
+                f[i][j] = Integer.MAX_VALUE;
+                if (j != 0) {
+                    f[i][j] = Math.min(f[i][j], f[i - 1][j - 1] + val);
+                }
+                if (j != i) {
+                    f[i][j] = Math.min(f[i][j], f[i - 1][j] + val);
+                }
+            }
+
+        }
+        for (int i = 0; i < n; i++) {
+            ans = Math.min(ans, f[n - 1][i]);
+
+        }
+        return ans;
     }
 }
