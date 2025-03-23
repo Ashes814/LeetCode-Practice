e1c58d10d9ea2905e023051673ce086755bebc81
Ashes814 <1534661514@qq.com>
2025-03-23 18:12:50 +0800

    pass leetcode 279

--- Java/LeetCodeJava/src/AlgBlog/Dynamic/DynamicCode.java
+++ Java/LeetCodeJava/src/AlgBlog/Dynamic/DynamicCode.java
@@ -134,4 +134,20 @@ public class DynamicCode {
         return res;
 
     }
+
+    public int numSquares(int n) {
+        int[] f = new int[n + 1];
+        f[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            f[i] = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                if (f[i - j * j] + 1 < f[i]) {
+                    f[i] = f[i - j * j] + 1;
+                }
+
+            }
+
+        }
+        return f[n];
+    }
 }
