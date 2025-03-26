2af5b4338a72a57887358dc557b4c9cdd7b6295b
Ashes814 <1534661514@qq.com>
2025-03-26 10:22:33 +0800

    pass leetcode 5

--- Java/LeetCodeJava/src/AlgBlog/Dynamic/DynamicCode.java
+++ Java/LeetCodeJava/src/AlgBlog/Dynamic/DynamicCode.java
@@ -215,20 +215,43 @@ public class DynamicCode {
     }
 
     public static String longestPalindrome(String s) {
-        int sLen = s.length();
-        if (sLen <= 1) {
-            return s;
-        }
+       int len = s.length();
+       if (len < 2) {
+           return s;
+       }
+       int maxLen = 1;
+       int begin = 0;
+       boolean[][] dp = new boolean[len][len];
+       for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+       }
 
-        int[] dp = new int[sLen];
-        dp[0] = 0;
-        for (int i = 1; i < sLen; i++) {
-            dp[i] = 0;
-            for (int j = 0; j < i; j++) {
+       char[] charArray = s.toCharArray();
+        for (int L = 2; L <= len; L++) {
+            for (int i = 0; i < len; i++) {
+                int j = L + i - 1;
+                if (j >= len) {
+                    break;
+                }
+
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen  = j - i + 1;
+                    begin = i;
+                }
 
             }
-
         }
+        return s.substring(begin, begin + maxLen);
     }
 }
 
