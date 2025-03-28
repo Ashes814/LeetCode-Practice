0836229062395aca4b09b3738404e9edc6c50809
Ashes814 <1534661514@qq.com>
2025-03-28 11:00:45 +0800

    implement dynamic code

--- Java/LeetCodeJava/src/AlgBlog/Dynamic/DynamicCode.java
+++ Java/LeetCodeJava/src/AlgBlog/Dynamic/DynamicCode.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class DynamicCode {
     public static void main(String[] args) {
-
+        longestCommonSubsequence("jianyang", "lijiang");
     }
 
     public int uniquePaths(int m, int n) {
@@ -299,6 +299,34 @@ public class DynamicCode {
         }
         return f[n] - 1;
     }
+
+    public static int longestCommonSubsequence(String A, String B) {
+
+            int m = A.length();
+            int n = B.length();
+            int i, j;
+            int[][] f = new int[2][n + 1];
+            int old, now = 0;
+            for (i = 0; i <= n; i++) {
+                f[now][i] = 0;
+            }
+            for (i = 1; i <= m; i++) {
+                old = now;
+                now = 1 - now;
+                for (j = 0; j <= n; j++) {
+                    f[now][j] = f[old][j];
+                    if (j > 0) {
+                        f[now][j] = Math.max(f[now][j], f[now][j - 1]);
+                    }
+                    if (j > 0 && A.charAt(i - 1) == B.charAt(j - 1)) {
+                        f[now][j] = Math.max(f[now][j], f[old][j - 1] + 1);
+                    }
+                }
+            }
+            return f[now][n];
+    }
+
+
 }
 
 
