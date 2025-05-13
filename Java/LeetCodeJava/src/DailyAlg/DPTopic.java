f7e2e832c9d0efd6244ca32f21e6a2ddf595491f
Ashes814 <1534661514@qq.com>
2025-05-13 11:08:41 +0800

    review dp

--- Java/LeetCodeJava/src/DailyAlg/DPTopic.java
+++ Java/LeetCodeJava/src/DailyAlg/DPTopic.java
@@ -2,7 +2,20 @@ package DailyAlg;
 
 public class DPTopic {
     public int uniquePaths(int m, int n) {
-        return search(m, n);
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] = dp[j - 1] + dp[j];
+            }
+        }
+
+        return dp[n - 1];
+
+
     }
 
     public int search(int m, int n) {
@@ -11,5 +24,23 @@ public class DPTopic {
         }
 
         return search(m - 1, n) + search(m, n - 1);
+    }
+
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][] f = new int[m][n];
+        f[0][0] = grid[0][0];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0 && j > 0) {
+                    f[i][j] = Math.min(f[i - 1][j], f[i][j - 1]) + grid[i][j];
+                } else if (j > 0) {
+                    f[i][j] = f[i][j - 1] + grid[i][j];
+                } else if (i > 0) {
+                    f[i][j] = f[i - 1][j] + grid[i][j];
+                }
+            }
+        }
+        return f[m-1][n-1];
     }
 }
