0065731917a3bf5120877feff584688794e8bed0
Ashes814 <1534661514@qq.com>
2025-07-31 17:13:39 +0800

    update leetcode 63

--- Java/LeetCodeJava/src/DailyAlg/DPTopic.java
+++ Java/LeetCodeJava/src/DailyAlg/DPTopic.java
@@ -214,4 +214,25 @@ public class DPTopic {
         return f[n];
     }
 
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        int[] dp = new int[m];
+        dp[0] = obstacleGrid[0][0] == 0 ? 1 : 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                    continue;
+                }
+
+                if (j - 1 >= 0 && obstacleGrid[i][j - 1] == 0) {
+                    dp[j] += dp[j - 1];
+                }
+
+            }
+
+        }
+        return dp[m - 1];
+    }
+
 }
