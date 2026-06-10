d19e5b1d335dcf5c0cba006590e898dd3b63a761
Ashes814 <1534661514@qq.com>
2026-06-10 16:10:07 +0800

    update c++ implementation

--- Java/LeetCodeJava/src/DailyAlg/LeetCode43.java
+++ Java/LeetCodeJava/src/DailyAlg/LeetCode43.java
@@ -35,3 +35,35 @@ public class LeetCode43 {
         return sb.length() == 0 ? "0" : sb.toString();
     }
 }
+
+
+//string multiply(string num1, string num2) {
+//        int m = num1.size();
+//        int n = num2.size();
+//
+//        vector<int> res(m+n, 0);
+//
+//        for (int i = m - 1; i >= 0; i--) {
+//            int n1 = num1[i] - '0';
+//            for (int j = n - 1; j>= 0; j--) {
+//                int n2 = num2[j] - '0';
+//
+//                int mul = n1 * n2;
+//                int sum = mul + res[i + j + 1];
+//
+//                res[i + j + 1] = sum % 10;
+//                res[i + j] += sum /10;
+//            }
+//        }
+//
+//        string sb = "";
+//        for (int num; res) {
+//            if (!(sb.empty() && num == 0)) {
+//                sb += to_string(num);
+//            }
+//        }
+//
+//        return sb.empty() ? "0": sb;
+//}
+
+
