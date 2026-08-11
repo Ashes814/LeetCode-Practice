9e2f9724a89d20fb76730f105157eacf95c9d634
Ashes814 <1534661514@qq.com>
2026-08-11 15:37:25 +0800

    pass leetcode 147

---
+++ Java/LeetCodeJava/src/DailyAlg/LeetCode147.java
@@ -0,0 +1,36 @@
+package DailyAlg;
+
+public class LeetCode147 {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode lastSorted = head, curr = head.next;
+        while (curr != null) {
+            if (lastSorted.val <= curr.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode prev = dummyHead;
+                while (prev.next.val <= curr.val) {
+                    prev = prev.next;
+                }
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+        return dummyHead.next;
+
+    }
+}
+
+//class ListNode {
+//    int val;
+//   ListNode next;
+//  ListNode() {}
+//  ListNode(int val) { this.val = val; }
+//  ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//}
