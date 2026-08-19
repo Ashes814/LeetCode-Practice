618f599a2a7de01b49babd79ea7cae093b70a773
Ashes814 <1534661514@qq.com>
2026-08-19 15:00:09 +0800

    pass leetcode 148

--- Java/LeetCodeJava/src/DailyAlg/LeetCode148.java
+++ Java/LeetCodeJava/src/DailyAlg/LeetCode148.java
@@ -9,45 +9,57 @@ public class LeetCode148 {
         sortList(test);
     }
 
-    public static ListNode sortList(ListNode head) {
+    public ListNode sortList(ListNode head) {
+        return sortList(head, null);
+    }
 
-
-        if (head == null || head.next == null) {
+    public ListNode sortList(ListNode head, ListNode tail) {
+        if (head == null) {
             return head;
         }
 
-        ListNode dummyHead = new ListNode();
-        dummyHead.next = head;
-
-        ListNode curNode = head;
-        ListNode prevNode = dummyHead;
-        ListNode nextNode = head.next;
-        ListNode finishNode = null;
-
-        while (dummyHead.next != finishNode) {
-            while (nextNode != finishNode) {
-                if (curNode.val > nextNode.val) {
-                    prevNode.next = nextNode;
-                    curNode.next = nextNode.next;
-                    nextNode.next = curNode;
-                    prevNode = nextNode;
-                    nextNode = curNode.next;
-                } else {
-                    prevNode = curNode;
-                    curNode = nextNode;
-                    nextNode = nextNode.next;
-                }
-            }
-            finishNode = curNode;
-
-            curNode = dummyHead.next;
-            prevNode = dummyHead;
-            nextNode = curNode.next;
+        if (head.next == tail) {
+            head.next = null;
+            return head;
         }
 
-        return dummyHead.next;
-
-
+        ListNode slow = head, fast = head;
+        while (fast != tail) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != tail) {
+                fast = fast.next;
+            }
+        }
+        ListNode mid = slow;
+        ListNode list1 = sortList(head, mid);
+        ListNode list2 = sortList(mid, tail);
+        ListNode sorted = merge(list1, list2);
+        return sorted;
     }
+
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 != null) {
+            temp.next = temp1;
+        }
+
+        if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
+    }
+
 
 }
